0092a33e0f2c28425d258b496822f0c467797c15
nazuna <nazuna11develop@gmail.com>
2020-03-19 18:21:44 +0900

    init commit

---
+++ parquet2html.py
@@ -0,0 +1,62 @@
+from fastparquet import ParquetFile
+import json
+
+def htmlFrame():
+    return """
+    <!doctype html>
+    <html>
+    <head>
+    <meta charset="utf-8">
+    <title>parquet2html</title>
+    <!--<link href="css/style.css" rel="stylesheet" type="text/css">-->
+    </head>
+    <body>
+    </body>
+    </html>
+    """
+
+def createListHtml(cname, ctype):
+    return f"""<tr>
+      <td>{cname}</td>
+      <td>{str(ctype)}</td>
+    </tr>"""
+
+def createTableHtml(fields, title, result_array=[]):
+    columns = []
+    for field in fields["fields"]:
+        if isinstance(field["type"], dict) and field["type"]["type"]=="struct":
+            createTableHtml(field["type"], field["name"], result_array)
+            column_type = "struct"
+        elif isinstance(field["type"], dict) and field["type"]["type"]=="array":
+            array_type = field["type"]["elementType"]
+
+            if isinstance(array_type, dict) and array_type["type"] == "struct":
+                array_type = "struct"
+                createTableHtml(field["type"]["elementType"], field["name"], result_array)
+            column_type = f"array<{array_type}>"
+        else:
+            column_type = field["type"]
+        column_html = createListHtml(field["name"], column_type)
+        columns.append(column_html)
+
+    result_array.append(f"""
+        <div id="info">
+        <h3>{title}</h3>
+        <table border="1">
+            <tr>
+              <th>neme</th>
+              <th>type</th>
+            </tr>
+            {"".join(columns)}
+        </table>
+        </div>
+        """)
+
+table = pf = ParquetFile('data/parquet/part-00000-4b0ffe1c-29e3-42c1-881a-686d08da8e37-c000.snappy.parquet')
+parent_schema = table.key_value_metadata["org.apache.spark.sql.parquet.row.metadata"]
+#pname_list = parent_schema.names
+#ptype_list = parent_schema.types
+parent_fields = json.loads(parent_schema)
+result_array = []
+result = createTableHtml(parent_fields, "root", result_array)
+print(result_array)
